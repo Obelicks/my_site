07596f5b131ef3feb5c695dd31a55e69da1b49c9
Emil Bløcher <bloechrr@gmail.com>
2023-09-01 15:22:24 +0200

    Update members

--- my_site/urls.py
+++ my_site/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('members.urls')),
+    path('', include('members.urls')), #sends us to the members/urls.py 
     path('admin/', admin.site.urls),
 ]
