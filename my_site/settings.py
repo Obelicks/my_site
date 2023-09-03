c395b135835bdda0c82fa5e9a4480d20c951c539
bloecher <bloechrr@gmail.com>
2023-09-03 12:12:39 +0200

    Add 404 site

--- my_site/settings.py
+++ my_site/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6zr1wvizq=u&5w62n&jvd-5k(#xjg-20zp(h0o1t9acm2*=h%o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
