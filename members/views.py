622d99ce181fd536b19f97e0b796b3aeab86f28e
Emil Bløcher <bloechrr@gmail.com>
2023-09-11 14:15:25 +0200

    Update master.html with style specification

--- members/views.py
+++ members/views.py
@@ -23,8 +23,12 @@ def main(request):
   return HttpResponse(template.render())
 
 def testing(request):
+  mymembers = Member.objects.all().values()
   template = loader.get_template('template.html')
   context = {
-    'fruits': ['Apple', 'Banana', 'Cherry'],   
+    'mymembers': mymembers,   
+    'x': ['Apple', 'Banana', 'Cherry'], 
+    'y': ['Apple', 'Banana', 'Cherry'], 
+    'firstname': 'Linus',
   }
   return HttpResponse(template.render(context, request))
